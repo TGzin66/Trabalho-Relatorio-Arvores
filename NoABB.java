95a2de9455fd8a122924fba033da8e4d227c68dc
TGzin66 <heron07emanuel@gmail.com>
2025-12-04 12:23:51 -0300

    Criação do No para ABB

---
+++ NoABB.java
@@ -0,0 +1,11 @@
+public class NoABB {
+  int valor;
+    NoABB esquerda;
+    NoABB direita;
+
+    public NoABB(int valor) {
+        this.valor = valor;
+        this.esquerda = null;
+        this.direita = null;
+    }
+}
